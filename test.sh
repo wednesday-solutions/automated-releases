276c7f2e5ee669cf093f18a825710d6db1a9c16c
aliarslanansari <aliarslan1620@gmail.com>
2020-10-01 16:53:32 +0530

    perf:faster than ever1

--- test.sh
+++ test.sh
@@ -1,12 +1,83 @@
-fix=''
-for i in $(git log --format=%h 21064e446701946cfe9bfe691385b136d596728b..dd2d50098047aed057b87b49bf0d1d69ee04d40d)
-do
-    IFS=":" read -a arr <<< $(git log --format=%B -n 1 $i)
-    if [ "${arr[0]}" == "fix" ]
-        then
-            fix+="${arr[1]}"
-    fi
-    echo "${fix}"  
-done
-
-    # msg+="&#8226; [$(git log --format=%B -n 1 $i)](https://github.com/${{ github.repository }}/commit/$i)<br/>"
+        msg=""
+          fix=""
+          build=""
+          ci=""
+          docs=""
+          feat=""
+          perf=""
+          refactor=""
+          style=""
+          test=""
+          for i in $(git log --format=%h 7abe829281b0c7be20718723fa1d286a309e3a84..25ac280c13415356e1856f308820cdd6a0d6dd70)
+          do
+            IFS=":" read -a arr <<< $(git log --format=%B -n 1 $i)
+              if [ "${arr[0]}" == "fix" ]
+                  then
+                      fix+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "feat" ]
+                  then
+                      feat+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "ci" ]
+                  then
+                      ci+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "docs" ]
+                  then
+                      docs+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "build" ]
+                  then
+                      build+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "perf" ]
+                  then
+                      perf+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "refactor" ]
+                  then
+                      refactor+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "style" ]
+                  then
+                      style+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+              if [ "${arr[0]}" == "test" ]
+                  then
+                      test+="&#8226; $i - ${arr[1]}<br/>"
+              fi
+          done
+          if [ ! -z "${fix}" ]
+            then
+              msg+="### Bug Fixes<br/>$fix"
+          fi
+          if [ ! -z "${feat}" ]
+              then
+              msg+="### New Features<br/>$feat"
+          fi
+          if [ ! -z "${ci}" ]
+              then
+              msg+="### Continuous Integration<br/>$ci"
+          fi
+          if [ ! -z "${docs}" ]
+              then
+              msg+="### Documentation<br/>$docs"
+          fi
+          if [ ! -z "${perf}" ]
+              then
+              msg+="### Performance Enhancement<br/>$perf"
+          fi
+          if [ ! -z "${refactor}" ]
+              then
+              msg+="### Refactored<br/>$refactor"
+          fi
+          if [ ! -z "${style}" ]
+              then
+              msg+="### Changed Style<br/>$style"
+          fi
+          if [ ! -z "${test}" ]
+              then
+              msg+="### Added Tests<br/>$test"
+          fi
+          echo $msg
