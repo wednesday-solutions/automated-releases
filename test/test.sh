d0581cfd3acf5978bb1606dc56917c2b640aebfb
aliarslanansari <aliarslan1620@gmail.com>
2020-10-05 14:15:25 +0530

    fix:added workflow for alpha, beta, prod releases

--- test/test.sh
+++ test/test.sh
@@ -1,25 +1,7 @@
-on:
-  push:
-    branches:
-      - personal/ali-arsalan
-
-name: Create Alpha Release
-
-jobs:
-  build:
-    name: Create Alpha Release
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v2
-      - run: |
-          git fetch --prune --unshallow --tags
-      - name: Get Commit Message
-        run: |
           declare -A cata=( [fix]="" [build]="" [ci]="" [docs]="" [feat]="" [perf]="" [refactor]="" [style]="" [test]="" [others]="") 
           declare -A cataTitle=( [fix]="<h5>Bug Fixes</h5>" [build]="<h5>Build</h5>" [ci]="<h5>Continuous Integration</h5>" [docs]="<h5>Documentation</h5>" [feat]="<h5>New Features</h5>" [perf]="<h5>Performance Enhancement</h5>" [refactor]="<h5>Refactored</h5>" [style]="<h5>Changed Style</h5>" [test]="<h5>Added Tests</h5>" [others]="<h5>Others</h5>") 
           msg=""
-          for i in $(git log --format=%h ${{ github.event.before }}..${{ github.event.after }})
+          for i in $(git log --format=%h 7e1dd3601b496fed45668cca1369624696feb8af..fc7f1db3903955734a5d44a54617e139e8eb3b6f)
           do
             IFS=":" read -r type cmmsg <<< $(git log --format=%B -n 1 $i)
             type="${type}" | xargs
@@ -30,8 +12,8 @@ jobs:
                 if [ "${type}" == "$i" ]
                 then
                     cata[$i]+="${text_msg}"
-                    flag=0
                     break
+                    flag=0
                 fi
             done
                 if [ $flag -eq 1 ]
@@ -50,24 +32,4 @@ jobs:
             then
                 msg+="${cataTitle[others]}${cata[others]}"
             fi
-            echo ::set-env name=COMMIT_MESSAGE::"${msg}"
-      - name: Bump version and push tag
-        run: |
-          cd "$GITHUB_WORKSPACE"
-          git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-          git config user.name "$GITHUB_ACTOR"
-          npm version patch
-          git push && git push --tags
-      - name: get-npm-version
-        id: package-version
-        uses: martinbeentjes/npm-get-version-action@master
-      - name: Create Alpha Release
-        uses: actions/create-release@latest
-        env:
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        with:
-          tag_name: ${{ steps.package-version.outputs.current-version}}-alpha
-          release_name: V${{ steps.package-version.outputs.current-version}}-alpha
-          body: ${{ env.COMMIT_MESSAGE }}
-          draft: false
-          prerelease: false
+          echo "${msg}"
